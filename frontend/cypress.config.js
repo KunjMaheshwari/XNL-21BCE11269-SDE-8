1a135ede892300c1580e254daf9a3f433928e929
Kunj Maheshwari <kunjm@outlook.com>
2025-03-14 11:38:40 +0530

    Updated the url.

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://usermanagement-sable-psi.vercel.app/",
     supportFile: false,
   },
 });
